ad217adaf4f281b151b884d6cf8d701360d65598
Matias Gimenez <matiasgimenez.dev@gmail.com>
2023-12-11 11:38:47 -0300

    add Footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,19 @@
-export const Footer = () => {
-	return <div>Footer</div>;
+import { FunctionComponent } from 'react';
+
+export const Footer: FunctionComponent = () => {
+	return (
+		<footer>
+			<hr />
+			<p className='w-full text-center pt-[20px] text-sm md:text-base'>
+				Built with 🧉 & <span className='text-rustyred'>❤</span> by
+				<a
+					className='ml-1 text-rustyred font-semibold cursor-pointer hover:underline'
+					href='https://github.com/matiasgimenezdev'
+					target='_blank'
+				>
+					Matías Giménez
+				</a>
+			</p>
+		</footer>
+	);
 };
