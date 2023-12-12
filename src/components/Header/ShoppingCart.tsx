6a8da8765a4434a81722c04342e9df0a6ba62a16
matiasgimenezdev <matiasgimenez.dev@gmail.com>
2023-12-12 11:46:31 -0300

    change shopping cart icon

--- src/components/Header/ShoppingCart.tsx
+++ src/components/Header/ShoppingCart.tsx
@@ -1,13 +1,13 @@
 import { FunctionComponent } from 'react';
 import { HeaderButton } from './HeaderButton';
-import { RiShoppingCart2Line, RiShoppingCart2Fill } from 'react-icons/ri';
+import { PiShoppingCart , PiShoppingCartFill } from 'react-icons/pi';
 
 export const ShoppingCart: FunctionComponent = () => {
 	return (
 		<li className='ml-3 md:ml-5'>
 			<HeaderButton
-				active={<RiShoppingCart2Fill />}
-				inactive={<RiShoppingCart2Line />}
+				active={<PiShoppingCartFill />}
+				inactive={<PiShoppingCart  />}
 			/>
 			{/* <MenuList/> */}
 		</li>
