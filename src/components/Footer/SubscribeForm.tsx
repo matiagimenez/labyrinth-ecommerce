2174f5e99ad8648c2dbd8ac8817149779bb8b6b8
matiasgimenezdev <matiasgimenez.dev@gmail.com>
2023-12-12 10:18:18 -0300

    modify opacitiy on hover

--- src/components/Footer/SubscribeForm.tsx
+++ src/components/Footer/SubscribeForm.tsx
@@ -25,7 +25,7 @@ export const SubscribeForm: FunctionComponent = () => {
 				/>
 				<button
 					type='submit'
-					className='ml-1 p-2 bg-rustyred border-2 border-rustyred text-white rounded hover:opacity-[.8] transition-opacity'
+					className='ml-1 p-2 bg-rustyred border-2 border-rustyred text-white rounded hover:opacity-[.85] transition-opacity'
 					onClick={(event) => {
 						event.preventDefault();
 						setEmail('');
