7a965520af80a41b98727bd6b0c88cf2a04ab3a8
matiasgimenezdev <matiasgimenez.dev@gmail.com>
2023-12-12 11:21:08 -0300

    add z-index & update buttons position

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,7 +2,7 @@ import { ShoppingCart } from './ShoppingCart';
 import { WishList } from './WishList';
 export const Header = () => {
 	return (
-		<header className='bg-cyan min-h-[90px] h-[10%] p-5 pl-3 fixed top-0 w-full sm:pl-6 md:min-h-[100px] shadow-md '>
+		<header className='bg-cyan z-10 min-h-[90px] h-[10%] p-5 pl-3 fixed top-0 w-full sm:pl-6 md:min-h-[100px] shadow-md '>
 			<h1 className='inline-block text-xl font-varela md:text-2xl'>
 				<a href='/'>
 					<img
@@ -13,7 +13,7 @@ export const Header = () => {
 					LABYRINTH
 				</a>
 			</h1>
-			<nav className='inline-block w-[40%] absolute top-9 right-5 sm:right-10 md:right-52'>
+			<nav className='inline-block w-[40%] absolute top-9 right-5 sm:right-10 md:right-32'>
 				<ul className='flex cont-right h-full items-bottom justify-end'>
 					<WishList />
 					<ShoppingCart />
