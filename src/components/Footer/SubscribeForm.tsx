0520d686487cea87e26049d668bb31801cb74452
matiasgimenezdev <matiasgimenez.dev@gmail.com>
2023-12-13 11:14:50 -0300

    refactor SubscribeForm component

--- src/components/Footer/SubscribeForm.tsx
+++ src/components/Footer/SubscribeForm.tsx
@@ -1,8 +1,25 @@
-import { FunctionComponent, useState } from 'react';
+import {
+	FunctionComponent,
+	useId,
+	useState,
+	FormEvent,
+	ChangeEvent,
+} from 'react';
 import { toast } from 'react-toastify';
 
 export const SubscribeForm: FunctionComponent = () => {
+	const id = useId();
 	const [email, setEmail] = useState<string>('');
+
+	const emailId = `email-${id}`;
+
+	function handleSubmit(event: FormEvent<HTMLFormElement>): void {
+		event.preventDefault();
+		if (!email) return;
+
+		toast.success('You successfully subscribed to the newsletter');
+		setEmail('');
+	}
 
 	return (
 		<section className='text-center mt-2'>
@@ -13,29 +30,32 @@ export const SubscribeForm: FunctionComponent = () => {
 				Get 10% off on your first order just by subscribir to our
 				newsletter
 			</p>
-			<form className='mt-3 mb-3'>
+			<form
+				className='mt-10 mb-3 relative ml-auto mr-auto'
+				onSubmit={handleSubmit}
+			>
+				<label
+					htmlFor={emailId}
+					className='mr-2 absolute -top-6 text-sm font-extralight'
+				>
+					Your e-mail address:
+				</label>
 				<input
 					type='email'
-					placeholder='Enter you email'
-					className='p-2 border-2 rounded w-[60%] sm:w-[400px] outline-rustyred border-cyan'
+					id={emailId}
+					name='email'
+					autoComplete='off'
+					className='p-2 border-2 rounded w-[60%] md:w-[500px] outline-rustyred border-cyan'
 					value={email}
-					onChange={(event) => {
-						setEmail(event.target.value);
+					onChange={(event: ChangeEvent<HTMLInputElement>) => {
+						setEmail(event.currentTarget.value);
 					}}
 				/>
-				<button
+				<input
 					type='submit'
 					className='ml-1 p-2 bg-rustyred border-2 border-rustyred text-white rounded hover:opacity-[.85] transition-opacity'
-					onClick={(event) => {
-						event.preventDefault();
-						setEmail('');
-						toast.success(
-							'You successfully subscribed to the newsletter'
-						);
-					}}
-				>
-					Subscribe
-				</button>
+					value='Subscribe'
+				/>
 			</form>
 			<p className='font-extralight text-md pb-5 md:text-base'>
 				You will be able to unsubscribe at any time
