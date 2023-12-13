1dbe0dca4e3b36f73debd9bc0028ebc5c6d5cfa6
matiasgimenezdev <matiasgimenez.dev@gmail.com>
2023-12-13 11:58:02 -0300

    change image size & text position

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -6,7 +6,7 @@ export const HomePage: FunctionComponent = () => {
 	return (
 		<>
 			<section className='relative flex flex-col items-center md:pt-8 md:flex-row md:items-start'>
-				<p className=' inline-block text-center text-5xl order-2 w-[100%] md:w-[40%] md:order-1 md:text-left xl:text-6xl md:p-8 md:pr-0 md:pt-24 lg:p-24 lg:pr-0 lg:pt-36'>
+				<p className=' inline-block text-center text-5xl order-2 w-[100%] md:w-[40%] md:order-1 md:text-left xl:text-7xl md:p-8 md:pr-0 md:pt-24 lg:p-24 lg:pr-0 lg:pt-36'>
 					All products{' '}
 					<span className='block font-bold text-rustyred mt-2'>
 						{' '}
@@ -19,13 +19,13 @@ export const HomePage: FunctionComponent = () => {
 						Shop now <FaArrowRightLong className='inline' />
 					</a>
 				</p>
-				<p className='absolute rotate-90 text-cyan -right-32 top-48 hidden font-bold text-[90px] opacity-[30%] -z-10 sm:block lg:text-[100px] xl:-right-24'>
+				<p className='select-none absolute rotate-90 text-cyan -right-32 top-48 hidden font-bold text-[90px] opacity-[30%] -z-10 sm:block lg:text-[100px] xl:right-0 xl:text-[110px] xl:top-56'>
 					35% OFF
 				</p>
 				<img
 					src='/images/laptop-homepage.png'
-					alt='Apple MacBook Pro M2'
-					className='order-1 w-[350px] h-[350px] inline-block transition-transform duration-500 hover:-rotate-6 sm:order-1 md:mt-2 md:w-[400px] md:h-[400px] lg:w-[500px] lg:h-[500px] '
+					alt='Laptop image'
+					className='order-1 select-none w-[350px] h-[350px] inline-block transition-transform duration-500 hover:-rotate-3 sm:order-1 md:mt-2 md:w-[400px] md:h-[400px] lg:w-[500px] lg:h-[500px] xl:w-[550px] xl:h-[550px]'
 				/>
 			</section>
 			<Catalog />
