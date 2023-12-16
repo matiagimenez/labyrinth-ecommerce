a45a587c91d363f52a41ad9b9d3a5acf787c9ae0
Matias Gimenez <matiasgimenez.dev@gmail.com>
2023-12-16 13:00:14 -0300

    change carousel styles & buttons

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -2,6 +2,7 @@ import { FunctionComponent, useState } from 'react';
 import { IoIosArrowForward, IoIosArrowBack } from 'react-icons/io';
 import { Image } from '../../types';
 import { CarouselItem } from '..';
+import { range } from '../../utils/range';
 
 type CarouselProps = {
 	images: Image[];
@@ -25,24 +26,23 @@ export const Carousel: FunctionComponent<CarouselProps> = ({ images }) => {
 	}
 
 	const buttonStyles = `
-		absolute bg-rustyred top-0 p-2 rounded-lg text-white text-xs
+		bg-rustyred rounded-lg text-white text-xs p-2 ml-1
 	`;
 
 	return (
-		<section className='mt-14'>
-			<h2 className='text-black px-5 text-lg  font-semibold lg:text-xl'>
+		<section className='md:mt-24 relative'>
+			<h2 className='text-black px-5 py-8 text-xl font-medium lg:text-2xl xl:text-3xl'>
 				Explore our{' '}
-				<span className='inline-block px-1 font-bold text-rustyred animate-bounce duration-1000 text-xl lg:text-3xl'>
+				<span className='pb-4 mt-2 inline-block px-1 font-bold text-rustyred animate-bounce duration-1000 text-3xl lg:text-4xl'>
 					latest products
-				</span>{' '}
-				and find the perfect one for you
+				</span>
 			</h2>
-			<nav className='relative'>
+			<nav className='absolute right-6 top-24 pt-2 md:top-24 md:right-16'>
 				<button
 					onClick={handleBackClick}
 					className={`${
 						current === 1 && 'opacity-0 cursor-default'
-					} ${buttonStyles} right-16`}
+					} ${buttonStyles}`}
 				>
 					<IoIosArrowBack />
 				</button>
@@ -50,12 +50,12 @@ export const Carousel: FunctionComponent<CarouselProps> = ({ images }) => {
 					onClick={handleForwardClick}
 					className={`${
 						current === images.length && 'opacity-0 cursor-default'
-					} ${buttonStyles} right-8`}
+					} ${buttonStyles}`}
 				>
 					<IoIosArrowForward />
 				</button>
 			</nav>
-			<ul className='flex items-center justify-center py-6 px-4 lg:gap-6'>
+			<ul className='flex w-full items-center justify-center py-8 px-4 gap-4 min-h-[400px] lg:gap-6'>
 				{images.map((image: Image) => {
 					return (
 						<CarouselItem
@@ -67,6 +67,23 @@ export const Carousel: FunctionComponent<CarouselProps> = ({ images }) => {
 					);
 				})}
 			</ul>
+			<p className='text-center'>
+				{range(1, images.length + 1).map((index) => {
+					return (
+						<span
+							key={index}
+							onClick={() => {
+								setCurrent(index);
+							}}
+							className={`cursor-pointer inline-block w-[50px] h-[6px] ml-0.5 rounded-lg ${
+								index === current
+									? 'bg-rustyred'
+									: 'border border-pink bg-transparent'
+							}`}
+						/>
+					);
+				})}
+			</p>
 		</section>
 	);
 };
--- src/components/Carousel/CarouselItem.tsx
+++ src/components/Carousel/CarouselItem.tsx
@@ -1,5 +1,7 @@
 import { FunctionComponent } from 'react';
 import { Image } from '../../types';
+// import { VscHeart, VscHeartFilled } from 'react-icons/vsc';
+import { VscHeart } from 'react-icons/vsc';
 
 type CarouselItemProps = {
 	image: Image;
@@ -14,34 +16,47 @@ export const CarouselItem: FunctionComponent<CarouselItemProps> = ({
 }) => {
 	const { src, alt, href, id } = image;
 
+	const styles: string = `
+		rounded-xl p-6 pt-2 border
+	`;
+
 	if (id === current) {
 		return (
-			<li>
-				<p>
-					<img
-						src={src}
-						alt={alt}
-						className='w-[250px] lg:w-[300px]'
-					/>
-					<a
-						href={href}
-						className='bg-rustyred text-white py-2 w-[90px] rounded ml-auto mr-auto text-xs text-center block hover:opacity-90'
-					>
-						See detail
-					</a>
-				</p>
+			<li className={`${styles} shadow-md block relative`}>
+				<img src={src} alt={alt} className='w-[220px] lg:w-[300px]' />
+				<p className='py-2 pb-4 font-medium text-center'>{alt}</p>
+				<a
+					href={href}
+					className='bg-rustyred text-white py-2 w-[120px] rounded ml-auto mr-auto text-xs text-center block hover:opacity-90'
+				>
+					See detail
+				</a>
+				<button className='absolute bottom-8 right-10 text-rustyred text-2xl'>
+					<VscHeart />
+				</button>
 			</li>
 		);
 	}
 
 	return (
-		<li>
+		<li
+			className={`${styles} cursor-pointer opacity-50 hidden md:block`}
+			onClick={() => handleItemClick(id)}
+		>
 			<img
 				src={src}
 				alt={alt}
-				className='w-[120px] cursor-pointer opacity-50 lg:w-[150px]'
-				onClick={() => handleItemClick(id)}
+				className='w-[120px] lg:w-[150px] ml-auto mr-auto block'
 			/>
+			<p className='py-2 opacity-30 pb-4 font-medium text-center'>
+				{alt}
+			</p>
+			<a
+				href={href}
+				className='bg-rustyred hidden text-white py-2 w-[90px] rounded ml-auto mr-auto text-xs text-center  hover:opacity-90'
+			>
+				See detail
+			</a>
 		</li>
 	);
 };
