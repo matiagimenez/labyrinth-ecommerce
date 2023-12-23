8d9e1dde10c916bac49372cd4076d661b76e7292
Matias Gimenez <matiasgimenez.dev@gmail.com>
2023-12-23 12:33:13 -0300

    add WishList context

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,30 @@
 import { useState } from 'react';
 import { PageLayout } from './layout';
 import { HomePage } from './pages/';
-import { ShoppingCart } from './types';
-import { ShoppingCartContext } from './context/';
+import { ShoppingCart, WishList } from './types';
+import { ShoppingCartContext, WishListContext } from './context/';
 
 function App() {
 	const [shoppingCart, setShoppingCart] = useState<ShoppingCart>({});
+	const [wishList, setWishList] = useState<WishList>([]);
 
 	function updateShoppingCart(products: ShoppingCart) {
 		setShoppingCart(products);
+	}
+
+	function updateWishList(products: WishList) {
+		setWishList(products);
 	}
 
 	return (
 		<ShoppingCartContext.Provider
 			value={{ shoppingCart, updateShoppingCart }}
 		>
-			<PageLayout>
-				<HomePage />
-			</PageLayout>
+			<WishListContext.Provider value={{ wishList, updateWishList }}>
+				<PageLayout>
+					<HomePage />
+				</PageLayout>
+			</WishListContext.Provider>
 		</ShoppingCartContext.Provider>
 	);
 }
