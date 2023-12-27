440564164e5e6835a6fab86d5426d642d3ae04ad
Matias Gimenez <matiasgimenez.dev@gmail.com>
2023-12-27 11:06:37 -0300

    add ErrorPage

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,3 +1,22 @@
+import { TbFaceIdError } from 'react-icons/tb';
+import { Link } from 'react-router-dom';
+
 export const ErrorPage = () => {
-	return <div>ErrorPage</div>;
+	return (
+		<section className='overflow-hidden justify-center relative flex flex-col items-center min-h-[480px]'>
+			<p className='text-rustyred text-[250px]'>
+				<TbFaceIdError />
+			</p>
+			<h2 className='text-rustyred font-bold text-4xl py-2'>ERROR 404</h2>
+			<p className='text-rustyred font-medium text-2xl'>
+				Sorry, the page not found
+			</p>
+			<Link
+				to='/'
+				className='text-white bg-rustyred rounded-3xl p-4 mt-4 px-12 hover:underline underline-offset-4'
+			>
+				Back to home
+			</Link>
+		</section>
+	);
 };
