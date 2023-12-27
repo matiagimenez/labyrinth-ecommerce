b12f1878229802eedfa802fca1bec5c7b38e1354
Matias Gimenez <matiasgimenez.dev@gmail.com>
2023-12-27 17:36:17 -0300

    fetch product by id

--- src/pages/ProductPage.tsx
+++ src/pages/ProductPage.tsx
@@ -1,3 +1,33 @@
+import { Navigate, useParams } from 'react-router-dom';
+import { ProductInformation } from '../components';
+import { ProductDetail } from '../components/Product/ProductDetail';
+import { useEffect, useState } from 'react';
+import { Product } from '../types';
+import { getProductById } from '../data/asyncProducts';
+
 export const ProductPage = () => {
-	return <div>ProductPage</div>;
+	const [product, setProduct] = useState<Product>();
+	const { productId } = useParams();
+
+	async function fetchProductData(id: string) {
+		const currentProduct: Product = await getProductById(id);
+		if (currentProduct) {
+			setProduct(currentProduct);
+		} else {
+			<Navigate to='/error' replace={true} />;
+		}
+	}
+
+	useEffect(() => {
+		if (productId) {
+			fetchProductData(productId);
+		}
+	}, [productId]);
+
+	return (
+		<>
+			<ProductDetail product={product} />
+			<ProductInformation product={product} />
+		</>
+	);
 };
