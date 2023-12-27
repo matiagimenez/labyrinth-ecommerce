3cbb8a38e2e76ef600a6429426ae1baf4d8e24fb
Matias Gimenez <matiasgimenez.dev@gmail.com>
2023-12-27 17:35:07 -0300

    fix key types

--- src/types/Product.ts
+++ src/types/Product.ts
@@ -11,54 +11,10 @@ export type Product = {
 	images: string[];
 	banner: string;
 	description: string;
-	features?: {
-		cpu: {
+	features: {
+		[key: string]: {
 			title: string;
-			text: string;
-		};
-		ram: {
-			title: string;
-			text: string;
-		};
-		screen: {
-			title: string;
-			text: string;
-		};
-		gpu: {
-			title: string;
-			text: string;
-		};
-		storage: {
-			title: string;
-			text: string;
-		};
-		connectivity: {
-			title: string;
-			text: string;
-		};
-		os: {
-			title: string;
-			text: string;
-		};
-		ports: {
-			title: string;
-			text: string;
-		};
-		other: {
-			title: string;
-			text: string;
-		};
-		model: {
-			title: string;
-			text: string;
-		};
-		dimensions: {
-			title: string;
-			text: string;
-		};
-		usage: {
-			title: string;
-			text: string;
+			text: string[];
 		};
 	};
 };
